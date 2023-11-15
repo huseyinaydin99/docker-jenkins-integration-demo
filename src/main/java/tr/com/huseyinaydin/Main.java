b14787a36439503beaa98d4a4c9e670ecba7f8d9
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-15 11:57:24 +0300

    first commit

--- src/main/java/tr/com/huseyinaydin/Main.java
+++ src/main/java/tr/com/huseyinaydin/Main.java
@@ -3,5 +3,8 @@ package tr.com.huseyinaydin;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Selamlar!");
+        int a = 10, b = 20, c = a + b;
+        System.out.println(c);
     }
 }
