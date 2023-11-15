451e648d3805d6b6fbecb816e167a32e47488cc0
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-11-15 12:24:44 +0300

    docker file command added

--- src/main/java/tr/com/huseyinaydin/Main.java
+++ src/main/java/tr/com/huseyinaydin/Main.java
@@ -3,7 +3,7 @@ package tr.com.huseyinaydin;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Selamlar!");
+        System.out.println("Selamlar! Nasılsınız?");
         int a = 10, b = 20, c = a + b;
         System.out.println(c);
         System.out.println(++c);
